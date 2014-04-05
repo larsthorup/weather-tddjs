a141aa27f265b655de9fab202ee772f5a78820b9
Lars Thorup <lars@zealake.com>
2014-04-04 20:57:52 -0700

    code coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     gruntConfig.jasmine = {
         src: {
-            src: 'src/**/*.js',
+            src: [
+                'src/**/*.js',
+                '!src/lib/*.js',
+            ],
             options: {
                 specs: 'test/**/*.test.js',
                 vendor: 'src/lib/**/*.js',
@@ -14,6 +17,24 @@ module.exports = function (grunt) {
         }
     };
     grunt.registerTask('test', ['jasmine:src']);
+
+    gruntConfig.jasmine.istanbul = {
+        src: gruntConfig.jasmine.src.src,
+        options : {
+            specs: gruntConfig.jasmine.src.options.specs,
+            vendor: gruntConfig.jasmine.src.options.vendor,
+            template: require('grunt-template-jasmine-istanbul'),
+            templateOptions: {
+                coverage: 'bin/coverage/coverage.json',
+                report: [
+                    {type: 'text-summary'},
+                    {type: 'html', options: {dir: 'output/coverage'}},
+                    {type: 'cobertura', options: {dir: 'output/cobertura'}}
+                ]
+            }
+        }
+    };
+    grunt.registerTask('cover', ['jasmine:istanbul']);
 
     grunt.initConfig(gruntConfig);
 };
